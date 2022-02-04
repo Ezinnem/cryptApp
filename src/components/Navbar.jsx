ca785cd29490183f6f58658fd8b1e1fd8b2b30ea
Ezinne Nnamani <nnamani.ezinne@gmail.com>
2022-02-05 00:06:07 +0100

    Implemented the Card for showing the Coin details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
+
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(undefined);
+  
+    useEffect(() => {
+      const handleResize = () => setScreenSize(window.innerWidth);
+  
+      window.addEventListener('resize', handleResize);
+  
+      handleResize();
+  
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    useEffect(() => {
+      if (screenSize <= 800) {
+        setActiveMenu(false);
+      } else {
+        setActiveMenu(true);
+      }
+    }, [screenSize]);
+  
     return (
         <div className="nav-container">
             <div className="logo-container">
@@ -23,9 +45,6 @@ const Navbar = () => {
                 </Menu.Item>
                 <Menu.Item icon={<FundOutlined />}>
                     <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-                </Menu.Item>
-                <Menu.Item icon={<MoneyCollectOutlined />}>
-                    <Link to="/exchanges">Exchanges</Link>
                 </Menu.Item>
                 <Menu.Item icon={<BulbOutlined />}>
                     <Link to="/news">News</Link>
