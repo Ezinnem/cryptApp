b5da0e4d3d29b0a860880d533ee6e1d9f4fbb52e
Ezinne Nnamani <nnamani.ezinne@gmail.com>
2022-02-05 22:49:19 +0100

    Updated the key value for coin cards

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -30,7 +30,7 @@ const Cryptocurrencies = ({ simplified }) => {
             )}
             <Row gutter={[32, 32]} className='crypto-card-container'>
                 {cryptos?.map((currency) => (
-                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.rank}>
                         <Link to={`/crypto/${currency.uuid}`}>
                             <Card title={`${currency.rank} .${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} />}  hoverable>
                                 <p>Price: {millify(currency.price)}</p>
